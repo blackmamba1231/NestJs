51df994b908fb33ffecf88e210400b051e11f749
sudhanshu tiwary <sudhanshukumar321456@gmail.com>
2024-12-14 23:49:18 +0530

    wlcm

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,10 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Module, MiddlewareConsumer } from '@nestjs/common'
 import { AuthMiddleware } from './auth.middleware';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+
 
 @Module({
   imports: [UserModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'), // Path to your public directory
-    }),
     ConfigModule.forRoot(), 
     MongooseModule.forRoot(process.env.MONGO_URI),
      ],
